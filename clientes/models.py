d05fb84b4698202c62c2db738c3aa51cb28a5720
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-03-28 22:00:13 -0300

    Alteração do nome do modelo de Clientes para Cliente e migração.

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Clientes(models.Model):
+class Cliente(models.Model):
     cpf = models.CharField(max_length=11, primary_key =True) # CPF é único para cada cliente Ex: 12345678900
     nome = models.CharField(max_length=100) # Nome do cliente Ex: João da Silva
     data_nascimento = models.DateField() # Data de nascimento do cliente Ex: 21-01-1990
