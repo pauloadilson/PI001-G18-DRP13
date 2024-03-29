61145376a434cf71351d936f4da1b646a82f087d
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-03-29 08:41:30 -0300

    Inclusão da view clientes

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from clientes.views import clientes_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clientes/', clientes_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
