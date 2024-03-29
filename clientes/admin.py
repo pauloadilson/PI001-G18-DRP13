af6a9dd990e4fb494e8767b3e04cf7e3b69573bc
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-03-28 22:17:48 -0300

    Registro da classe Cliente no arquivo admin.py do app Cliente

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente
 
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('cpf', 'nome', 'data_nascimento', 'telefone_whatsapp', 'telefone')
+    search_fields = ('cpf', 'nome')
+
+admin.site.register(Cliente, ClienteAdmin) # Registra o modelo Cliente no admin do Django da Classe Cliente e da configuração ClienteAdmin
