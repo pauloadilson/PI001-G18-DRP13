15e4a363cfa56bd375d0daef0d4a459ffc4dac30
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-03-30 15:15:04 -0300

    Criação do template de clientes.html e configuração do views.py para receber contexto.

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def clientes_view(request):
-    return HttpResponse('Meus clientes')
+    return render(
+        request, 
+        'clientes.html', 
+        {
+            'clientes': [{
+                'cpf': 35307319860, 
+                'nome': 'João da Silva', 
+                'data_nascimento': '1990-01-21', 
+                'telefone_whatsapp': 81999998888, 
+                'telefone': 81999998888, 
+            },
+            {
+                'cpf': 21841043885, 
+                'nome': 'Roberto da Silva', 
+                'data_nascimento': '1981-02-23', 
+                'telefone_whatsapp': 81999998888, 
+                'telefone': 81999998888, 
+            }],
+        }
+    )
+            
