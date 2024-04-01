f75b6d51c53c6484ff3d717664954b3ab7f81ff4
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-04-01 20:25:23 -0300

    Criação da template base e pagina index do app. Alteração dos arquivos url, views, template clientes para apresentar os dados extraidos do BD.

--- app/urls.py
+++ app/urls.py
@@ -19,8 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from clientes.views import clientes_view
+from clientes.views import index
+
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('clientes/', clientes_view),
+    path('clientes/', clientes_view, name='clientes'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- clientes/views.py
+++ clientes/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
+from clientes.models import Cliente
 
 # Create your views here.
 
 def clientes_view(request):
+    clientes = Cliente.objects.all()
+
     return render(
         request, 
         'clientes.html', 
         {
-            'clientes': [{
-                'cpf': 35307319860, 
-                'nome': 'João da Silva', 
-                'data_nascimento': '1990-01-21', 
-                'telefone_whatsapp': 81999998888, 
-                'telefone': 81999998888, 
-            },
-            {
-                'cpf': 21841043885, 
-                'nome': 'Roberto da Silva', 
-                'data_nascimento': '1981-02-23', 
-                'telefone_whatsapp': 81999998888, 
-                'telefone': 81999998888, 
-            }],
+            'title': 'Clientes',
+            'clientes':clientes
         }
     )
-            
+
+def index(request):
+    return render(
+        request, 
+        'index.html', 
+        {
+            'title': 'Home',
+        }
+    )
