57fa88fd32b0481cec5d57e55e30049315be4839
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-04-01 21:36:32 -0300

    Implementação da busca no navbar por cpf do cliente.

--- clientes/views.py
+++ clientes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from clientes.models import Cliente
-
 # Create your views here.
 
 def clientes_view(request):
-    clientes = Cliente.objects.all()
+    clientes = Cliente.objects.all() # .order_by('nome') '-nome' para ordem decrescente
+    busca = request.GET.get('busca') # busca é o nome da chave de busca
+    if busca:
+        clientes = clientes.filter(cpf__icontains=busca).order_by('nome')
 
     return render(
         request, 
