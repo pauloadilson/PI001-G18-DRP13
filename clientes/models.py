2ff4868b18ace487d98c1ebc58dfb92c176f9652
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-04-06 10:53:12 -0300

    Criação do models para exigências e recursos.

--- clientes/models.py
+++ clientes/models.py
@@ -9,7 +9,7 @@ class Cliente(models.Model):
     telefone = models.CharField(max_length=11, blank=True, null=True) # Telefone do cliente Ex: 81999998888
 
     def __str__(self) -> str:
-        return f'{self.nome}, {self.data_nascimento}, {self.cpf}'  # Retorna o nome do cliente e o CPF do cliente
+        return f'{self.cpf}, {self.nome}, {self.data_nascimento}, {self.telefone_whatsapp}, {self.telefone}'  # Retorna o nome do cliente e o CPF do cliente
     
 class Estado(models.Model):
     id = models.AutoField(primary_key=True) # ID do estado
@@ -39,3 +39,30 @@ class Requerimento(models.Model):
 
     def __str__(self) -> str:
         return f'{self.servico.nome}: {self.requerente_titular.nome}, {self.requerente_titular.cpf}, {self.requerente_titular.data_nascimento}' # Retorna o nome do cliente e a data do requerimento
+
+class Recurso(models.Model):
+    id = models.AutoField(primary_key=True) # ID do recurso
+    NB = models.ForeignKey(Requerimento, on_delete=models.PROTECT, related_name='NB_recurso') # Relacionamento com o modelo Requerimento
+    protocolo = models.CharField(max_length=20) # Número do protocolo do recurso
+    data = models.DateField() # Data do recurso
+    prazo_em_dias = 30 # Prazo para resposta do recurso
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT, related_name='estado_recurso') # Estado do recurso Ex: Pendente, Concluído
+    observacao = models.TextField() # Observações do recurso
+
+    def __str__(self) -> str:
+        return f'{self.requerimento.servico.nome}: {self.requerimento.requerente_titular.nome}, {self.requerimento.requerente_titular.cpf}, {self.requerimento.requerente_titular.data_nascimento}' # Retorna o nome do cliente e a data do recurso
+    
+class Exigência(models.Model):
+    id = models.AutoField(primary_key=True) # ID da exigência
+    NB = models.ForeignKey(Requerimento, on_delete=models.PROTECT, related_name='NB_exigencia') # Relacionamento com o modelo Requerimento
+    protocolo = models.CharField(max_length=20) # Número do protocolo da exigência
+    data = models.DateField() # Data da exigência
+    prazo_em_dias = 30 # Prazo para resposta da exigência
+    natureza = models.ForeignKey('Natureza', on_delete=models.PROTECT, related_name='natureza_exigencia') # Natureza da exigência Ex: Documentação, Informação
+
+class Natureza(models.Model):
+    id = models.AutoField(primary_key=True) # ID da natureza
+    nome = models.CharField(max_length=100) # Nome da natureza Ex: Documentação, Informação
+
+    def __str__(self) -> str:
+        return f'{self.nome}' # Retorna o nome da natureza
