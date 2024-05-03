67493c075cc8663685a1979ab35c1747e16039e8
Manoel Junior <manuelitojunior@gmail.com>
2024-05-03 19:47:02 -0300

    Implementação Banco Postgres

--- app/settings.py
+++ app/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": config("ENGINE"),
+        "NAME": config("NAME"),
+        "USER": config("USER"),
+        "PASSWORD": config("PASSWORD"),
+        "HOST": config("HOST"),
+        "PORT": config("PORT"),
     }
 }
 
