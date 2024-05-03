7044ca9e53eea7482fb225e43f1768d0314722f6
Manoel Junior <manuelitojunior@gmail.com>
2024-05-03 19:24:09 -0300

    Ajustes nos apps instalados

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "clientes",
+    "clientes.apps.ClientesConfig",
     "django_bootstrap5",
     "crispy_forms",
     "crispy_bootstrap5",
