b0f58102531e733487ab5b11a017b1f493b7be70
Manoel Junior <manuelitojunior@gmail.com>
2024-05-17 10:05:01 -0300

    configuração do static_root

--- app/settings.py
+++ app/settings.py
@@ -128,8 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
