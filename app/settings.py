efb8b7a71a0dd77bb0695b515e942269dc9031d8
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-05-28 20:49:54 -0300

    heroku settings

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['127.0.0.1', 'cp-prev-cb423f74017d.herokuapp.com']
 
 # Application definition
 
