e4321da6547ef70a38c2a8144ea18274f4c7c717
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-06-06 14:33:49 -0300

    private media only

--- app/settings.py
+++ app/settings.py
@@ -175,12 +175,6 @@ if S3_ENABLED:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 if not LOCAL_SERVE_MEDIA_FILES:
-    PUBLIC_MEDIA_DEFAULT_ACL = 'public-read'
-    PUBLIC_MEDIA_LOCATION = 'media/public'
-
-    MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'app.utils.storage_backends.PublicMediaStorage'
-
     PRIVATE_MEDIA_DEFAULT_ACL = 'private'
     PRIVATE_MEDIA_LOCATION = 'media/private'
     PRIVATE_FILE_STORAGE = 'app.utils.storage_backends.PrivateMediaStorage'
--- app/utils/storage_backend.py
+++ app/utils/storage_backend.py
@@ -1,13 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class PublicMediaStorage(S3Boto3Storage):
-    """Used to store & serve dynamic media files with no access expiration"""
-    location = settings.PUBLIC_MEDIA_LOCATION
-    default_acl = settings.PUBLIC_MEDIA_DEFAULT_ACL
-    file_overwrite = False
-
-
 class PrivateMediaStorage(S3Boto3Storage):
     """
     Used to store & serve dynamic media files using access keys
